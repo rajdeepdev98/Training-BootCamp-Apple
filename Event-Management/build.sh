e262e589b5276b738aa044175f1e38ab4b9a54e8
Rajdeep Deb <rajdeep.dev98@gmail.com>
2024-11-19 12:09:01 +0530

    Added docker-compose and build script

---
+++ Event-Management/build.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/rajdeepdev98/Training-BootCamp-Apple.git
+cd Training-BootCamp-Apple/
+
+cd Event-Management
+cd play-event-service
+docker build -t play-event-service:latest .
+
+cd ..
+cd akka-event-service
+docker build -t akka-event-service:latest .
+
+cd ..
+
+docker network create kafka-network
+
+
